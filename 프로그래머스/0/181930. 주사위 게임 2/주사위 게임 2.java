f4435bd68f76203f0c03b7c5e5925af05b53537f
choi-nyeong <131933011+choi-nyeong@users.noreply.github.com>
2024-07-21 17:40:11 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,13 +1,19 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        int answer = 0;
+        int answer = 1;
         
-        if(a!=b&&b!=c&&a!=c){
-            answer = a+b+c;
-        }else if(a==b&&b==c){
-            answer = (a+b+c)*((a*a)+(b*b)+(c*c))*((a*a*a)+(b*b*b)+(c*c*c));
-        }else{
-            answer = (a+b+c)*((a*a)+(b*b)+(c*c));
+        int count = 1;
+        
+        if(a==b||b==c||a==c){
+            count++;
+        }
+        
+        if(a==b&&a==c){
+            count++;
+        }
+        
+        for(int i=1; i<=count;i++){
+            answer *=(Math.pow(a,i)+Math.pow(b,i)+Math.pow(c,i));
         }
         
         return answer;
